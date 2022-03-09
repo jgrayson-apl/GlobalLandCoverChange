b826840211de59468bccb67dbdd53df05ce6beb9
john <jgrayson@esri.com>
2022-03-08 17:14:33 -0800

    fix raster functions list

--- app/Main.js
+++ app/Main.js
@@ -11,7 +11,7 @@
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
- limitations under the License.​
+ limitations under the License.
  */
 
 define([
@@ -799,7 +799,7 @@ define([
 
         // name, description //
         const menuItems = rasterFunctionInfos.reduce((items, rasterFunctionInfo) => {
-          if (rasterFunctionInfo.name !== "None") {
+          if (labelByRendererName.has(rasterFunctionInfo.name)) {
 
             const menuItemNode = domConstruct.create('div', {
               className: 'renderer-menuitem dropdown-link',
